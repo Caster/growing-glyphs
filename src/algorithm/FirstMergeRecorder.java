fd98d693a5d40a3061991c4b5055c267e95558d5
Thom Castermans <thom.castermans@gmail.com>
2017-11-13 14:34:33 +0100

    Use stream API for first merge recording
    This seems to be more efficient than the custom parallelization.

--- src/algorithm/FirstMergeRecorder.java
+++ src/algorithm/FirstMergeRecorder.java
@@ -1,16 +1,19 @@
 package algorithm;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+import java.util.stream.Collector.Characteristics;
 
 import datastructure.Glyph;
 import datastructure.events.Event;
 import datastructure.events.GlyphMerge;
 import datastructure.growfunction.GrowFunction;
-import utils.Utils.Timers;
 
 public class FirstMergeRecorder {
 
@@ -23,14 +26,10 @@ public class FirstMergeRecorder {
      */
     private GrowFunction g;
     /**
-     * First time at which a merge event with {@link #from} is recorded so far.
+     * Container that records when the first merge occurs, and which glyph(s)
+     * will merge with {@link from} at that point in time.
      */
-    private double minAt;
-    /**
-     * Set of glyphs that touch {@link #from} at time {@link #minAt}. In
-     * practice this will almost always contain just a single glyph.
-     */
-    private Set<Glyph> minGlyphs;
+    private FirstMerge merge;
 
 
     /**
@@ -40,8 +39,7 @@ public class FirstMergeRecorder {
     public FirstMergeRecorder(GrowFunction g) {
         this.from = null;
         this.g = g;
-        this.minAt = Double.MAX_VALUE;
-        this.minGlyphs = new HashSet<>(1);
+        this.merge = new FirstMerge();
     }
 
 
@@ -63,13 +61,22 @@ public class FirstMergeRecorder {
      * @param l Logger to log events to, can be {@code null}.
      */
     public void addEventsTo(Queue<Event> q, Logger l) {
-        for (Glyph with : minGlyphs) {
-            q.add(new GlyphMerge(from, with, minAt));
+        for (Glyph with : merge.glyphs) {
+            q.add(new GlyphMerge(from, with, merge.at));
             if (l != null) {
                 l.log(Level.FINEST, "-> merge at {0} with {1}",
-                        new Object[] {minAt, with});
+                        new Object[] {merge.at, with});
             }
         }
+    }
+
+    public Collector<Glyph, FirstMerge, FirstMerge> collector() {
+        return Collector.of(
+                () -> new FirstMerge(),
+                (m, g) -> m.accept(g),
+                (a, b) -> a.combine(b),
+                Characteristics.CONCURRENT,
+                Characteristics.UNORDERED);
     }
 
     /**
@@ -80,24 +87,7 @@ public class FirstMergeRecorder {
      */
     public void from(Glyph from) {
         this.from = from;
-        this.minAt = Double.MAX_VALUE;
-        this.minGlyphs.clear();
-    }
-
-    /**
-     * Record a possible merge event.
-     *
-     * @param candidate Glyph that the {@code from} glyph could merge with.
-     */
-    public void record(Glyph candidate) {
-        double at = g.intersectAt(from, candidate);
-        if (at < minAt) {
-            minAt = at;
-            minGlyphs.clear();
-            minGlyphs.add(candidate);
-        } else if (at == minAt) {
-            minGlyphs.add(candidate);
-        }
+        this.merge.reset();
     }
 
     /**
@@ -112,9 +102,7 @@ public class FirstMergeRecorder {
      * @param upto Index up to but excluding which glyphs will be recorded.
      */
     public void record(Glyph[] glyphs, int from, int upto) {
-        for (int i = from; i < upto; ++i) {
-            record(glyphs[i]);
-        }
+        record(Arrays.stream(glyphs, from, upto));
     }
 
     /**
@@ -126,12 +114,75 @@ public class FirstMergeRecorder {
      * @param glyphs Set of glyphs to record.
      */
     public void record(Set<Glyph> glyphs) {
-        Timers.start("set to array");
-        Glyph[] arr = glyphs.parallelStream()
-                .filter((glyph) -> glyph.alive && glyph != from)
-                .toArray(Glyph[]::new);
-        Timers.stop("set to array");
-        record(arr, 0, arr.length);
+        record(glyphs.parallelStream());
+    }
+
+    /**
+     * {@link #record(Glyph) Record} all glyphs in the given stream, as long as
+     * they are {@link Glyph#alive} and not {@link #from}.
+     *
+     * This method may use parallelization to speed up recording.
+     *
+     * @param glyphs Stream of glyphs to record.
+     */
+    public void record(Stream<Glyph> glyphs) {
+        merge.combine(glyphs.parallel()
+            .filter((glyph) -> glyph.alive && glyph != from)
+            .collect(collector()));
+    }
+
+
+    /**
+     * Container for collecting the first merge; when will it happen and which
+     * glyphs are involved, aside from {@link FirstMergeRecorder#from}.
+     */
+    private class FirstMerge {
+
+        /**
+         * First time at which a merge event with {@link FirstMergeRecorder#from}
+         * is recorded so far.
+         */
+        private double at;
+        /**
+         * Set of glyphs that touch {@link FirstMergeRecorder#from} at time
+         * {@link #at}. In practice this will almost always contain just a
+         * single glyph.
+         */
+        private Set<Glyph> glyphs;
+
+
+        public FirstMerge() {
+            this.at = Double.MAX_VALUE;
+            this.glyphs = new HashSet<>(1);
+        }
+
+        public void accept(Glyph candidate) {
+            double at = g.intersectAt(from, candidate);
+            if (at < this.at) {
+                this.at = at;
+                this.glyphs.clear();
+                this.glyphs.add(candidate);
+            } else if (at == this.at) {
+                this.glyphs.add(candidate);
+            }
+        }
+
+        public FirstMerge combine(FirstMerge that) {
+            if (that.at < this.at) {
+                this.at = that.at;
+                this.glyphs.clear();
+                this.glyphs.addAll(that.glyphs);
+            } else if (that.at == this.at) {
+                this.glyphs.addAll(that.glyphs);
+            }
+            return this;
+        }
+
+        public void reset() {
+            at = Double.MAX_VALUE;
+            glyphs.clear();
+        }
+
     }
 
 }
