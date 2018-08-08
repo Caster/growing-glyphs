5fc52618cca281d57db5fd3894158af308acc46e
Thom Castermans <thom.castermans@gmail.com>
2018-08-08 16:53:04 +0200

    Change big glyph generation and data set
    The big glyph has greater weight, the small glyphs have equal and
    smaller weight now. The small glyphs are placed not in a circle, but a
    square around the big glyph.

--- src/algorithm/glyphgenerator/BigGlyph.java
+++ src/algorithm/glyphgenerator/BigGlyph.java
@@ -1,12 +1,14 @@
 package algorithm.glyphgenerator;
 
 import datastructure.Glyph;
+import utils.Utils;
 
 /**
  * This generator generates a very heavy glyph in the very center, with after
  * that a ring of uniformly distributed glyphs around it. The weight of the big
- * glyph will be the fourth power of the maximum value in the range given by
- * {@link GlyphGenerator#WEIGHT_RANGE}.
+ * glyph will be a high power of the maximum value in the range given by
+ * {@link GlyphGenerator#WEIGHT_RANGE}. All other glyphs will have the minimum
+ * weight from the same range assigned to them.
  */
 public class BigGlyph extends GlyphGenerator {
 
@@ -14,12 +16,12 @@ public class BigGlyph extends GlyphGenerator {
      * Percentage of rectangle size that points need to be away from the big
      * glyph in the center, at least.
      */
-    public static final double MIN_DIST_SQ = 0.2;
+    public static final double MIN_DIST = 0.2;
     /**
      * Percentage of rectangle size that points can be away from the big glyph
      * in the center, at most.
      */
-    public static final double MAX_DIST_SQ = 0.4;
+    public static final double MAX_DIST = 0.4;
 
 
     public BigGlyph() {
@@ -32,21 +34,20 @@ public class BigGlyph extends GlyphGenerator {
 
         // the first glyph returned will be the big glyph
         if (i == 1) {
-            return new Glyph(0, 0, WEIGHT_RANGE[1] * WEIGHT_RANGE[1] *
-                    WEIGHT_RANGE[1] * WEIGHT_RANGE[1], true);
+            return new Glyph(0, 0, (int) Math.pow(WEIGHT_RANGE[1], 8), true);
         }
 
         // other glyphs are in a ring around the big one
         double x, y, r;
         double size = Math.min(rect.getWidth(), rect.getHeight());
-        double minR_SQ = (MIN_DIST_SQ * size) * (MIN_DIST_SQ * size);
-        double maxR_SQ = (MAX_DIST_SQ * size) * (MAX_DIST_SQ * size);
+        double minR = MIN_DIST * size;
+        double maxR = MAX_DIST * size;
         do {
             x = rand.nextDouble() * rect.getWidth() + rect.getX();
             y = rand.nextDouble() * rect.getHeight() + rect.getY();
-            r = x * x + y * y;
-        } while (r < minR_SQ || maxR_SQ < r);
-        return new Glyph(x, y, randN(), true);
+            r = Utils.chebyshev(x, y, 0, 0);
+        } while (r < minR || maxR < r);
+        return new Glyph(x, y, WEIGHT_RANGE[0], true);
     }
 
 }
